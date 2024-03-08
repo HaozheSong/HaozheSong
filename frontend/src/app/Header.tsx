0ed7fd466afff9c143e5a9283090f86fd1b68322
HaozheSong <im.haozhesong@gmail.com>
2024-03-08 17:03:59 -0500

    0.0.2 responsive header

--- frontend/src/app/Header.tsx
+++ frontend/src/app/Header.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import * as HeaderIcon from './HeaderIcon'
 import { usePathname } from 'next/navigation'
+import { useState } from 'react'
 
 interface HeaderLink {
   text: string
@@ -23,26 +24,83 @@ const HEADER: Header = {
   ]
 }
 
+const defaultHeaderBgColor = ' bg-slate-900'
+const defaultHeaderTextColor = 'text-sky-400'
+const activeHeaderTextColor = 'text-orange-400'
+const hoverHeaderTextColor = 'hover:text-orange-500'
+
 export default function Header () {
+  const [collapsed, setCollapsed] = useState(false)
   return (
-    <header className='flex justify-between h-12 px-4 bg-slate-900 text-sky-400'>
-      <span className='flex flex-wrap content-center text-2xl'>
-        {HEADER.title}
-      </span>
-      <span className='flex flex-wrap content-center'>
-        {HEADER.links.map(link => (
-          <HeaderLink link={link} key={link.text} />
-        ))}
-      </span>
+    <header className={`${defaultHeaderBgColor} ${defaultHeaderTextColor}`}>
+      <div className='flex justify-between h-12 px-4' id='headerTitleRow'>
+        <HeaderTitle />
+        <InlineNav />
+        <CollapseToggler collapsed={collapsed} setCollapsed={setCollapsed} />
+      </div>
+      <CollapsedNav collapsed={collapsed} />
     </header>
+  )
+}
+
+function HeaderTitle () {
+  return (
+    <span className='flex flex-wrap content-center text-2xl'>
+      {HEADER.title}
+    </span>
+  )
+}
+
+function InlineNav () {
+  return (
+    <nav className='hidden sm:flex sm:flex-wrap sm:content-center'>
+      {HEADER.links.map(link => (
+        <HeaderLink link={link} key={link.text} />
+      ))}
+    </nav>
+  )
+}
+
+function CollapseToggler ({
+  collapsed,
+  setCollapsed
+}: {
+  collapsed: boolean
+  setCollapsed: React.Dispatch<React.SetStateAction<boolean>>
+}) {
+  const togglerBaseClass = 'flex flex-wrap content-center transition sm:hidden'
+  const togglerCollapsedClass = `${togglerBaseClass} ${activeHeaderTextColor} rotate-90`
+  const togglerFoldedClass = `${togglerBaseClass}`
+  return (
+    <span
+      className={collapsed ? togglerCollapsedClass : togglerFoldedClass}
+      onClick={() => setCollapsed(!collapsed)}
+    >
+      {HeaderIcon.TogglerIcon}
+    </span>
+  )
+}
+
+function CollapsedNav ({ collapsed }: { collapsed: boolean }) {
+  const baseClass =
+    `absolute flex flex-col w-full px-4 pb-2 gap-y-2 transition duration-300 ` +
+    `${defaultHeaderBgColor} ${defaultHeaderTextColor} sm:hidden`
+  const collapsedClass = `${baseClass} opacity-95`
+  const foldedClass = `${baseClass} opacity-0`
+  return (
+    <nav className={collapsed ? collapsedClass : foldedClass}>
+      {HEADER.links.map(link => (
+        <HeaderLink link={link} key={link.text} />
+      ))}
+    </nav>
   )
 }
 
 function HeaderLink ({ link }: { link: HeaderLink }) {
   const pathname = usePathname()
   const isActive = pathname === link.href
-  const baseLinkClass = 'mr-4 hover:text-orange-500'
-  const activeLinkClass = baseLinkClass + ' text-orange-400'
+  const baseLinkClass = `mr-4 ${hoverHeaderTextColor}`
+  const activeLinkClass = `${baseLinkClass} ${activeHeaderTextColor}`
   return (
     <Link
       href={link.href}
